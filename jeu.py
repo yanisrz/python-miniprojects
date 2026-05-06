a3e90ad0554be722cebd8532c3dc73278900c8df
yanis R-Z <yanis.rz@gmail.com>
2026-05-06 20:36:06 +0200

    stable version

--- jeu.py
+++ jeu.py
@@ -2,22 +2,23 @@ import random
 import time 
 prix = random.randint(1, 50)
 debut_chrono = time.time()
-devine=""
+devine="     Devines le prix entre 1 et 50: "
 compteur: int = 0
-print(prix)
+print( devine )
 while devine != prix:
     compteur += 1
-    devine = input("     Devines le prix entre 1 et 50: \n     ")
+    devine = input("     \n     ")
     if devine.isdigit() == False:
         print("     !!Ce n'est pas un nombre, essaye encore!!😡\n")
-        devine = input("     reessaie:\n ")
         continue
     devine = int(devine)
     if devine < prix:
         print("     C'est trop petit !!")
+        print("     reessaie encore")
     elif devine > prix:
         print("     C'est trop grand !!")
-    
+        print("     reessaie encore")
+
 fin_chrono = time.time()
 chrono = int(fin_chrono-debut_chrono)
 print(f"     Bravo tu as trouve le prix en {compteur} tentatives et en {chrono}s!")
