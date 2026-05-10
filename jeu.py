028508e433c775a81a631da55f43c2c3cbfc08b1
yanis R-Z <yanis.rz@gmail.com>
2026-05-10 15:00:03 +0200

    add minute-seconde

--- jeu.py
+++ jeu.py
@@ -1,39 +1,51 @@
 import random
 import time 
-
-niveau = input("     Choisis ton niveau de difficulté (facile = 1, difficile = 2): ")
-
+#differentes librairies utilisées
+#on demande a l'utilisateur de choisir le niveau
+niveau = input("     Choisis ton niveau de difficulté (facile = 1, difficile = 2) : ")
+# le prix est generer en fonction du iveau choisi
 if niveau == "1":
     prix = random.randint(1, 50)
     grand_prix = 50
 elif niveau == "2":
-    prix = random.randint(1, 100)
+    prix = random.randint(1, 500)
     grand_prix = 100
 else:
-    print("     Niveau de difficulté pas valide, le jeu se lance en mode facile par defaut")
+    print("     Niveau de difficulté pas valide, le jeu se lance en mode facile par défaut")
     prix = random.randint(1,5) 
     grand_prix = 50
 
-
-
 debut_chrono = time.time()
-devine="     Devines le prix entre 1 et " + str(grand_prix) + ": "
+#on ajoute le grand prix dans le texte
+devine = (f"     Devine le prix entre 1 et {grand_prix} : ")
 compteur: int = 0
-print( devine )
+print( devine) 
+#on debute une boucle tant que "prix" est different de "devine" zu debut elle l'est car ce n'est meme pas un nombre entier 
 while devine != prix:
     compteur += 1
+    # on demande à l'utilisateur de deviner
     devine = input("     \n     ")
+    #on verifie que "devine" est un nombre entier
     if devine.isdigit() == False:
-        print("     !!Ce n'est pas un nombre, essaye encore!!😡\n")
+        print("     !!Ce n'est pas un nombre, essaie encore !!😡\n")
+        #on remonte au haut de la boucle
         continue
+    
+    #on cnvertie " devine" en entier pour povoir le comparer
     devine = int(devine)
     if devine < prix:
         print("     C'est trop petit !!")
-        print("     reessaie encore")
+        print("     Réessaie encore")
     elif devine > prix:
         print("     C'est trop grand !!")
-        print("     reessaie encore")
+        print("     Réessaie encore")
 
 fin_chrono = time.time()
 chrono = int(fin_chrono-debut_chrono)
-print(f"     Bravo tu as trouve le prix en {compteur} tentatives et en {chrono}s!")
+#on convertie les secondes en minutes et secondes
+minutes, secondes = divmod(chrono, 60)
+
+if minutes == 0:
+     print(f"     Bravo tu as trouvé le prix en {compteur} tentatives en {secondes}s !")
+else:
+    print(f"     Bravo tu as trouvé le prix en {compteur} tentatives et en {minutes}m {secondes}s !")
